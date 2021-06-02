bbc2d0ebb71166074ce22e232c3d2ab844e6f53b
Alexander Silanov <silanov.alexander@gmail.com>
2021-06-02 13:55:03 +0200

    fixed

--- js/main.js
+++ js/main.js
@@ -1,38 +1,24 @@
-/* eslint-disable prefer-const */
 // Функция, возвращающая случайное целое число из переданного диапазона включительно.
 
 /* описание метода взято с https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random. Пока не совсем разобрался с функцией- почему прибавляем 1 и min,но еще подумаю*/
 
-// eslint-disable-next-line prefer-const
-let min = 0;
-// eslint-disable-next-line prefer-const
-let max = 200;
-let text = 'Где-то ошибка!';
 
-// eslint-disable-next-line no-shadow
-function getRandomNumber(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  if(min <= max) {
-    return text;
+const getRandomNumber = function (min, max) {
+  if (min < 0 || max <= min) {
+    return 'Где-то ошибка!';
   }
   return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-getRandomNumber(min, max);
+};
+
+getRandomNumber();
+
 
 // Функция для проверки максимальной длины строки.
 
-// eslint-disable-next-line no-unused-vars
-let comment = '';
-let message = false;
-
-function textLong() {
-  if(comment.length > 140) {
-    return message;
+const textLong = function (comment, long) {
+  if (comment.length > long) {
+    return false;
   }
-  return comment;
-}
+  return true;
+};
 textLong();
-
-/* а через тернарный оператор ведь можно вот так? или не стоит?:
- let textLong = (comment.length > 140) ? comment : message; */

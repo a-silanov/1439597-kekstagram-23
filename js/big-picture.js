b5f66dc4b7fab0dd1a081cd29f019ae64db983fa
Alexander Silanov <silanov.alexander@gmail.com>
2021-07-19 23:07:55 +0200

    add

--- js/big-picture.js
+++ js/big-picture.js
@@ -1,8 +1,9 @@
 import {onEscKeyDown, hideElement} from './util.js';
 import {posts} from './data.js';
-import {checkCommentsNumber, renderComments} from './comment.js';
-import {clearComments} from './storage.js';
 
+const COMMENTS_LOAD_STEP = 5;
+const PHOTO_WIDTH = 35;
+const PHOTO_HEIGHT = 35;
 const photos = document.querySelectorAll('.picture');
 const bigPicture = document.querySelector('.big-picture');
 const bigPictureImg = bigPicture.querySelector('img');
@@ -12,6 +13,8 @@ const socialDescription = bigPicture.querySelector('.social__caption');
 const commentsLoader = bigPicture.querySelector('.comments-loader');
 const socialCommentCount = bigPicture.querySelector('.social__comment-count');
 const btnBigPictureClose = bigPicture.querySelector('.big-picture__cancel');
+const socialComments = document.querySelector('.social__comments');
+const commentsFragment = document.createDocumentFragment();
 const uploadForm = document.querySelector('.img-upload__form');
 const uploadOverlay = uploadForm.querySelector('.img-upload__overlay');
 const commentInput = bigPicture.querySelector('.social__footer-text');
@@ -25,12 +28,61 @@ const openPopup = (photo) => {
   hideElement(commentsLoader);
   hideElement(socialCommentCount);
   bigPictureImg.src = photo.url;
-  checkCommentsNumber(photo.comments.length);
   commentsCount.textContent = photo.comments.length;
   likesCount.textContent = photo.likes;
   socialDescription.textContent = photo.description;
-  renderComments(photo.comments);
-};
+  socialComments.innerHTML = '';
+  const generateCommentsList = (userComments) => {
+    userComments.forEach(({avatar, name, message}) => {
+      const comment = document.createElement('li');
+      const commentImg = document.createElement('img');
+      const commentText = document.createElement('p');
+
+      comment.classList.add('social__comment');
+      commentImg.classList.add('social__picture');
+      commentText.classList.add('social__text');
+      commentImg.src = avatar;
+      commentImg.alt = name;
+      commentImg.width = PHOTO_WIDTH;
+      commentImg.height = PHOTO_HEIGHT;
+      commentText.textContent = message;
+      comment.appendChild(commentImg);
+      comment.appendChild(commentText);
+      commentsFragment.appendChild(comment);
+    });
+    socialComments.appendChild(commentsFragment);
+  };
+
+
+
+  const commentsCounter = 0;
+  let currentCommentsNumber = COMMENTS_LOAD_STEP;
+
+  const initialComments = comments.slice(commentsCounter, currentCommentsNumber);
+  socialCommentCount.firstChild.textContent = `${initialComments.length} из  `;
+  generateCommentsList(initialComments);
+
+  commentsLoader.addEventListener('click', () => {
+    const newCommentsNumber = currentCommentsNumber + COMMENTS_LOAD_STEP;
+    const additionalComments = comments.slice(currentCommentsNumber, newCommentsNumber);
+    currentCommentsNumber = newCommentsNumber;
+    generateCommentsList(additionalComments);
+
+    const renderedCommentsLength = document.querySelectorAll('.social__comment').length;
+
+    if (comments.length === renderedCommentsLength) {
+      commentsLoader.classList.add('hidden');
+    }
+    socialCommentCount.firstChild.textContent = `${renderedCommentsLength} из  `;
+  });
+
+  if (comments.length > COMMENTS_LOAD_STEP) {
+    socialCommentCount.classList.remove('hidden');
+    commentsLoader.classList.remove('hidden');
+  } else {
+    socialCommentCount.classList.add('hidden');
+    commentsLoader.classList.add('hidden');
+  }
 
 photos.forEach( (photo, i) => {
   photo.addEventListener('click', () => {
@@ -47,7 +99,6 @@ btnBigPictureClose.addEventListener('click', (evt) => {
   document.body.classList.remove('modal-open');
   commentInput.value = '';
   uploadFile.value = '';
-  clearComments();
 });
 
 document.addEventListener('keydown', onEscKeyDown);
